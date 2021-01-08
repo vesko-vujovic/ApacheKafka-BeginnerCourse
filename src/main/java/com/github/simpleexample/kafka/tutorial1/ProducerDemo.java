45b68559adb0a4d7f61087593e7e969ae727f076
vesko-vujovic <veskovujovic@yahoo.com>
2021-01-08 12:15:22 +0100

    Initial commit

---
+++ src/main/java/com/github/simpleexample/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,37 @@
+package com.github.simpleexample.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world 2");
+
+        // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+        // close the data
+        producer.close();
+    }
+}
